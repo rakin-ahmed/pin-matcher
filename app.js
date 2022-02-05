a9308e632187351613d55657164a5fd66e13565f
rakin-ahmed <rakin1912@gmail.com>
2022-02-05 21:24:42 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
 function clicked(digit) {
-    var current=document.getElementById('display').value
-    var newDisplay=current+digit
-    document.getElementById('display').value=newDisplay
+    var current = document.getElementById('display').value
+    var newDisplay = current + digit
+    document.getElementById('display').value = newDisplay
     removeComment()
 }
 function erase() {
-    document.getElementById('generateDisplay').value=""
-    document.getElementById('display').value=""
+    document.getElementById('generateDisplay').value = ""
+    document.getElementById('display').value = ""
     removeComment()
 }
 function deleteDigit() {
-    var current= document.getElementById('display').value
-    var current= Array.from(current)
+    var current = document.getElementById('display').value
+    var current = Array.from(current)
     current.pop()
-    var current2=[]
+    var current2 = []
     for (let i = 0; i < current.length; i++) {
-        current2.push(parseInt(current[i]))      
+        current2.push(parseInt(current[i]))
     }
-    var current2=current.toString();
-    var current2= current2.replaceAll(",","")
-    document.getElementById('display').value=current2
+    var current2 = current.toString();
+    var current2 = current2.replaceAll(",", "")
+    document.getElementById('display').value = current2
     removeComment()
 }
 function generate() {
-    var generate=Math.random()*10000
-    var generate=generate.toFixed(0)
-    if (generate<1000) {
-        var generate=parseInt(generate)+1000
-       document.getElementById('generateDisplay').value=generate
+    var generate = Math.random() * 10000
+    var generate = generate.toFixed(0)
+    if (generate < 1000) {
+        var generate = parseInt(generate) + 1000
+        document.getElementById('generateDisplay').value = generate
     }
-    document.getElementById('generateDisplay').value=generate
-    document.getElementById('display').value=""
+    document.getElementById('generateDisplay').value = generate
+    document.getElementById('display').value = ""
     removeComment()
 }
 function submit() {
-    var generate= document.getElementById('generateDisplay').value
-    var display= document.getElementById('display').value
+    var generate = document.getElementById('generateDisplay').value
+    var display = document.getElementById('display').value
     console.log(typeof generate, generate);
     console.log(typeof display, display);
-    if (generate>0 && display>0) {
-        if (generate==display) {
-            document.getElementById('correct').style.display="block";
-        }else{
-            document.getElementById('false').style.display="block";
-            
+    if (generate > 0 && display > 0) {
+        if (generate == display) {
+            document.getElementById('correct').style.display = "block";
+            document.getElementById('false').style.display = "none";
+        } else {
+            document.getElementById('correct').style.display = "none";
+            document.getElementById('false').style.display = "block";
+
         }
     }
 }
 function removeComment() {
-    document.getElementById('correct').style.display="none";
-    document.getElementById('false').style.display="none";
+    document.getElementById('correct').style.display = "none";
+    document.getElementById('false').style.display = "none";
 }
